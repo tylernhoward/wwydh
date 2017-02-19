557e7999f71c6653fa3512859af87ceafb15d2b1
alecdavid95 <amyers24@students.towson.edu>
2017-02-19 17:57:34 -0500

    Add files via upload

--- login/change.php
+++ login/change.php
@@ -1,7 +1,7 @@
 <?php
 
 include("../helpers/conn.php");
-//require("wwydh/helpers/sendgrid-php/sendgrid-php/sendgrid-php.php");
+require("../helpers/sendgrid-php/sendgrid-php/sendgrid-php.php");
 // Was the form submitted?
 if (isset($_POST["ForgotPassword"])) {
 	
@@ -24,7 +24,7 @@ if (isset($_POST["ForgotPassword"])) {
 	
 	if ($userExists["email"])
 	{
-		
+		/*
 		// Create a unique salt. This will never leave PHP unencrypted.
 		$salt = "498#2D83B631%3800EBD!801600D*7E3CC13";
 
@@ -39,20 +39,20 @@ if (isset($_POST["ForgotPassword"])) {
 		$mailbody = "Dear user,\n\nIf this e-mail does not apply to you please ignore it. It appears that you have requested a password reset at our website www.wwydh.com\n\nTo reset your password, please click the link below. If you cannot click it, please paste it into your web browser's address bar.\n\n" . $pwrurl . "\n\nThanks,\nThe Administration";
 		mail("alecdavid95@comcast.net", "www.wwydh.com - Password Reset", $mailbody, $headers);
 		echo "Your password recovery key has been sent to your e-mail address.";
+		*/
 		
-		/*
 		$from = new SendGrid\Email("Example User", "test@example.com");
 		$subject = "Sending with SendGrid is Fun";
-		$to = new SendGrid\Email("Example User", "alecdavid95@comcast.net");
+		$to = new SendGrid\Email("Example User", "test@example.com");
 		$content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
 		$mail = new SendGrid\Mail($from, $subject, $to, $content);
-		$apiKey = "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"
+		$apiKey = getenv('SENDGRID_API_KEY');
 		$sg = new \SendGrid($apiKey);
 		$response = $sg->client->mail()->send()->post($mail);
 		echo $response->statusCode();
 		echo $response->headers();
 		echo $response->body();
-		*/
+		
 	}
 	else
 		echo "No user with that e-mail address exists.";
