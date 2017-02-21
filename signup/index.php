9b0b7f2f52ede123777bc982657a8eb72e18fdd1
alecdavid95 <amyers24@students.towson.edu>
2017-02-21 17:46:15 -0500

    js src changed to check

--- signup/index.php
+++ signup/index.php
@@ -44,7 +44,7 @@ if($result){
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script src="../helpers/globals.js" type="text/javascript"></script>
     <script src="scripts.js" type="text/javascript"></script>
-	<script type="text/JavaScript" src="submit.js"></script>
+	<script type="text/JavaScript" src="check.js"></script>
   </head>
   <body>
     <div class="width">
