3168896810ddce430eda5077c1a53fe7781f0db3
tylernhoward <tylernhoward@gmail.com>
2017-02-21 15:33:21 -0500

    Add files via upload

--- contact/sendemail.php
+++ contact/sendemail.php
@@ -7,25 +7,31 @@ if (isset($_POST["submit"])) {
 	if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
 		$email = $_POST["email"];
 
-
-		$name=$POST_["name"];
-		$message=$POST_["message"];
-
-		$from = new SendGrid\Email($name, $email);
-		$subject = "Contact from user";
-		$to = new SendGrid\Email("WWYDH", "wwydh2017@gmail.com"]);
-		$content = new SendGrid\Content($message);
-		$mail = new SendGrid\Mail($from, $subject, $to, $content);
-		$apiKey = getenv('SENDGRID_API_KEY');
-		$sg = new \SendGrid($apiKey);
-		$response = $sg->client->mail()->send()->post($mail);
-
-		echo "Thank you for contacting the team at WWYDH. \n\n We will get back to you shortly.";
-
-
 	}else{
 		echo "email is not valid";
 		exit;
 	}
+
+
+			$name=$POST_["name"];
+			$message=$POST_["message"];
+
+			$apiKey = getenv('SENDGRID_API_KEY');
+			$sg = new SendGrid($apiKey);
+
+			$mail = new SendGrid\Email();
+			$mail
+			->addTo('wwydh2017@gmail.com')
+			->setFrom($email)
+			->setSubject($name)
+			->setText($message);
+
+
+			$sg->send($mail);
+
+			echo "Thank you for contacting the team at WWYDH. \n\n We will get back to you shortly.";
+
+
+
 }
 ?>
