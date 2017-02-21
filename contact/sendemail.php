1f19bd698d6883654b525bf73b08e95bd1c4229f
tylernhoward <tylernhoward@gmail.com>
2017-02-21 13:42:01 -0500

    Update sendemail.php

--- contact/sendemail.php
+++ contact/sendemail.php
@@ -17,7 +17,7 @@ echo "Thank you, message sent";
 wait(5);
 header("Location: ../home/index.php");
 */
-
+echo "1";
 	// Harvest submitted e-mail address
 	if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
 		$email = $_POST["email"];
@@ -26,6 +26,7 @@ header("Location: ../home/index.php");
 		echo "email is not valid";
 		exit;
 	}
+echo "2";
 
 		$from = new SendGrid\Email($_POST["name"], $_POST["email"]);
 		$subject = "Contact from user: " . $POST_["name"] . "";
@@ -34,6 +35,7 @@ header("Location: ../home/index.php");
 		$mail = new SendGrid\Mail($from, $subject, $to, $content);
 		$apiKey = getenv('SENDGRID_API_KEY');
 		$sg = new \SendGrid($apiKey);
+		echo "3"
 		$response = $sg->client->mail()->send()->post($mail);
 		
 		echo "Thank you for contacting the team at WWYDH. \n\n We will get back to you shortly.";
