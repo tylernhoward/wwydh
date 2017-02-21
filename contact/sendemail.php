d5dd147fbfb8c28bc100895acb9a531a8b468a02
tylernhoward <tylernhoward@gmail.com>
2017-02-21 13:27:34 -0500

    still trying to make email work.

--- contact/sendemail.php
+++ contact/sendemail.php
@@ -1,6 +1,8 @@
 
 <?php
 require("../helpers/sendgrid-php/sendgrid-php/sendgrid-php.php");
+
+/*
 $sendgrid = new SendGrid(getenv('SENDGRID_API_KEY');
 $email    = new SendGrid\Email();
 
@@ -14,4 +16,31 @@ $sendgrid->send($email);
 echo "Thank you, message sent";
 wait(5);
 header("Location: ../home/index.php");
+*/
+
+if (isset($_POST["submit"])) {
+
+	// Harvest submitted e-mail address
+	if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+		$email = $_POST["email"];
+
+	}else{
+		echo "email is not valid";
+		exit;
+	}
+
+		$from = new SendGrid\Email($_POST["name"], $_POST["email"]);
+		$subject = "Contact from user: " . $POST_["name"] . "";
+		$to = new SendGrid\Email("Example User", $userExists["email"]);
+		$content = new SendGrid\Content($POST_["message"]);
+		$mail = new SendGrid\Mail($from, $subject, $to, $content);
+		$apiKey = getenv('SENDGRID_API_KEY');
+		$sg = new \SendGrid($apiKey);
+		$response = $sg->client->mail()->send()->post($mail);
+
+		echo "Thank you for contacting the team at WWYDH. \n\n We will get back to you shortly.";
+    wait(5);
+    header("Location: ../home/index.php");
+
+}
 ?>
