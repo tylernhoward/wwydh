72915732fa2e8dc5d668fd7f5a30a1cddf8c2ca0
tylernhoward <tylernhoward@gmail.com>
2017-02-21 14:01:40 -0500

    Update sendemail.php

--- contact/sendemail.php
+++ contact/sendemail.php
@@ -1,21 +1,6 @@
 <?php
 require("../helpers/sendgrid-php/sendgrid-php/sendgrid-php.php");
 
-/*
-$sendgrid = new SendGrid(getenv('SENDGRID_API_KEY');
-$email    = new SendGrid\Email();
-
-$email->addTo("wwydhContact@gmail.com")
-      ->setFrom($_POST['email'])
-      ->setSubject($_POST['name'])
-      ->setHtml($_POST['message']);
-
-$sendgrid->send($email);
-
-echo "Thank you, message sent";
-wait(5);
-header("Location: ../home/index.php");
-*/
 echo "1";
 	// Harvest submitted e-mail address
 	if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
