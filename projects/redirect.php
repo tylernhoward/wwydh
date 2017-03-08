3314eefb9db90a05a86eaa73ff431fa707e54b82
Taylor <tayham1996@gmail.com>
2017-03-07 20:17:35 -0500

    Redirect Test

--- projects/redirect.php
+++ projects/redirect.php
@@ -6,15 +6,16 @@
 	$_SESSION["projid"] = $project_id;
 	echo $_SESSION["projid"];
 
-	
+
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="UTF-8" />
+		<meta http-equiv="refresh" content="3; url=https://wwydh-2017.herokuapp.com/lwkanban/" /> //hardcoded for now
 		<title>WWYDH Project Manager</title>
 	</head>
 	<body>
-	<a href="../lwkanban">Tasks</a>
+	<a href="../lwkanban">Your browser will redirect you to Project Manager view in 3 seconds. If it does not then click this.</a>
 	</body>
 </html>
