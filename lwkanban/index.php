0beed684cf82c25ea0f223faae1b3bfdf18dd1d2
tylernhoward <tylerhoward@trailBlazer.local>
2017-03-07 21:01:31 -0500

    Merge branch 'Alec_Kanban_Test'
    # Conflicts:
    #	contact/sendemail.php
    #	lwkanban/LICENSE.md
    #	lwkanban/README.md
    #	lwkanban/composer.json
    #	lwkanban/css/style-ie7-8.css
    #	lwkanban/css/style.css
    #	lwkanban/data.json
    #	lwkanban/index.html
    #	lwkanban/js/core.js
    #	lwkanban/js/jquery.dragsort.min.js
    #	lwkanban/js/jquery.min.js
    #	lwkanban/js/settings.js
    #	lwkanban/preview/normal.png
    #	lwkanban/preview/select.png
    #	lwkanban/server.php
    #	projects/index.php
    #	projects/indexOLD.php
    #	projects/styles_new.css

--- lwkanban/index.php
+++ lwkanban/index.php
@@ -8,6 +8,7 @@
 		<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400|Montserrat:400,700" rel="stylesheet" />
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 		<link href="css/style.css" rel="stylesheet" type="text/css" />
+		<link href="../helpers/header_footer.css" type="text/css" rel="stylesheet" />
 
 		<!-- Scripts -->
 		<script type="text/javascript" src="js/jquery.min.js"></script>
@@ -21,6 +22,50 @@
 		<![endif]-->
 	</head>
 	<body>
+	<div class="width">
+			<div id="nav">
+	            <div class="nav-inner width clearfix <?php if (isset($_SESSION['user'])) echo 'loggedin' ?>">
+	                <a href="../home">
+	                    <div id="logo"></div>
+	                    <div id="logo_name">What Would You Do Here?</div>
+	                    <div class="spacer"></div>
+	                </a>
+	                <div id="user_nav" class="nav">
+	                    <?php if (!isset($_SESSION["user"])) { ?>
+	                        <ul>
+	                            <a href="../login"><li>Log in</li></a>
+	                            <a href="../signup"><li>Sign up</li></a>
+	                            <a href="../contact"><li>Contact</li></a>
+	                        </ul>
+	                    <?php } else { ?>
+	                        <div class="loggedin">
+	                            <span class="click-space">
+	                                <span class="chevron"><i class="fa fa-chevron-down" aria-hidden="true"></i></span>
+	                                <div class="image" style="background-image: url(../helpers/user_images/<?php echo $_SESSION["user"]["image"] ?>);"></div>
+	                                <span class="greet">Hi <?php echo $_SESSION["user"]["first"] ?>!</span>
+	                            </span>
+
+	                            <div id="nav_submenu">
+	                                <ul>
+	                                    <a href="../dashboard"><li>Dashboard</li></a>
+	                                    <a href="../profile"><li>My Profile</li></a>
+	                                    <a href="../helpers/logout.php?go=home"><li>Log out</li></a>
+	                                </ul>
+	                            </div>
+	                        </div>
+	                    <?php } ?>
+	                </div>
+	                <div id="main_nav" class="nav">
+	                    <ul>
+	                        <a href="../locations"><li>Locations</li></a>
+	                        <a href="../ideas"><li>Ideas</li></a>
+	                        <a href="../plans"><li>Plans</li></a>
+	                        <a href="../projects" class="active"><li>Projects</li></a>
+	                    </ul>
+	                </div>
+	            </div>
+	        </div>
+		</div>
 		<!-- Modal -->
 		<div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" id="helpModal">
 			<div class="modal-dialog" role="document">
@@ -58,16 +103,12 @@
 			</header>
 
 			<h2>WWYDH Project Manager - Roadmap</h2>
-<<<<<<< HEAD
-			<div id="board"></div>
-
-=======
 			<?php
-			$project_id = $GET['id'];
-			<!-- echo $project_id; -->
+			session_name( 'kanban' );
+			session_start();
+			// echo $_SESSION["projid"];
 			?>
 			<div id="board"></div>
->>>>>>> Alec_Kanban_Test
 			<h2>Tags</h2>
 			<div id="navigation" class="navigation"></div>
 		</div>
---
+++ projects/redirect.php
@@ -0,0 +1,21 @@
+<?php
+	$project_id = $_GET['id'];
+	// echo $project_id;
+	session_name( 'kanban' );
+	session_start();
+	$_SESSION["projid"] = $project_id;
+	// echo $_SESSION["projid"];
+
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="refresh" content="3; url=https://wwydh-2017.herokuapp.com/lwkanban/" /> <!-- hardcoded for now -->
+		<title>WWYDH Project Manager</title>
+	</head>
+	<body>
+	<a href="../lwkanban">Your browser will redirect you to Project Manager view in 3 seconds. If it does not then click this.</a>
+	</body>
+</html>
