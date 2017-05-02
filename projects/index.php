03986dec12667b67e6c9588b3f40f419048d1dfd
alecdavid95 <amyers24@students.towson.edu>
2017-05-02 17:43:52 -0400

    Add files via upload

--- projects/index.php
+++ projects/index.php
@@ -240,13 +240,19 @@ $( function() {
 							<div class="location">
 								<div class="plan-buttons options btn-group">
 									<?php
-										$manquery = "SELECT * FROM manager_of WHERE plan_id = '" . $planrow['id'] . "' ";
+										if (isset($_SESSION['user'])){
+										$manquery = "SELECT * FROM manager_of WHERE plan_id = '" . $planrow['id'] . "' AND user_id = '" . $_SESSION['user']['id'] . "' ";
 										$allmanage = $conn->query($manquery);
-										while($manage = $allmanage->fetch_assoc()){
+										 if ($allmanage->num_rows > 0) {
+											$indicator = 1;
+										}
+										else
+											$inidicator = 0;
+										}
 										/*if user is manager display tasks if not display become a project manager*/
 										if (!isset($_SESSION["user"])){ ?>
 											<div class="btn op-1"><a href="../login">login to edit task progress</a></div>
-										<?php } elseif (isset($_SESSION["user"]) && $_SESSION["user"]["manager"] == 1 && $_SESSION["user"]["id"] == $manage["user_id"] ){ ?>
+										<?php } elseif (isset($_SESSION["user"]) && $_SESSION["user"]["manager"] == 1 && $indicator = 1){ ?>
 											<div class="btn op-1"><a href="redirect.php?id=<?php echo $projectsrow['id']; ?>">Edit Task Progress</a></div>
 										<?php } else { ?>
 											<div class="btn op-1"><a href="tasktable.php?id=<?php echo $planrow['id']; ?>">See Task Progress</a></div>
@@ -268,7 +274,7 @@ $( function() {
 								<div style="clear: both"></div>
 							</div>
 
-						<?php } ?>
+							<?php } ?>
 					</div>
 		 	<?php }}
 			?>
